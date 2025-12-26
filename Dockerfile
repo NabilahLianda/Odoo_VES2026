130c1e4ba067c52d318a918e15cc779bfb6b88cd
NabilahLianda <nabilahl.1610@gmail.com>
2025-12-26 13:28:31 +0700

    New Modul

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM odoo:18
 USER root
 RUN apt-get update && apt-get install -y netcat-openbsd
 COPY odoo.conf /etc/odoo/odoo.conf
+COPY addons /mnt/extra-addons
 
 USER odoo
 CMD odoo \
---
+++ addons/custom_theme/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Custom Theme Color",
+    "version": "1.0",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "custom_theme/static/src/scss/theme.scss",
+        ],
+    },
+}
